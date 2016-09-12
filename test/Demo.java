ecf4cab04f33501dc73b4e5fe8a66f2486deca99
greatitman <agiletiger@qq.com>
2016-09-12 16:24:00 +0800

    read apk info by aapt

---
+++ test/Demo.java
@@ -0,0 +1,27 @@
+import com.sinaapp.msdxblog.apkUtil.entity.ApkInfo;
+import com.sinaapp.msdxblog.apkUtil.utils.ApkUtil;
+import com.sinaapp.msdxblog.apkUtil.utils.IconUtil;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try {
+//			String apkpath = "D:\\DefaultApkTempSaveFolder\\3G安卓市场\\com.jiubang.market.apk";
+			String apkpath = "D:\\SkyProxyService.apk";
+			if (args.length > 0) {
+				apkpath = args[0];
+			}
+			ApkInfo apkInfo = new ApkUtil().getApkInfo(apkpath);
+			// 打印获取到的信息
+			System.out.println(apkInfo);
+			// 获取Icon并保存到指定位置
+//			IconUtil.extractFileFromApk(apkpath, apkInfo.getApplicationIcon(), "D:\\DefaultApkTempSaveFolder\\3G安卓市场\\crawler\\icon.png");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
